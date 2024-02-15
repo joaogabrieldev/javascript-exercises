053b468c3fd3fff3a1eb11d631746c8ca03f3b1c
João Gabriel <dev.joaogabriel@gmail.com>
2024-02-14 21:42:34 -0300

    add 019/03

--- 03-numbers/exercicio18.js
+++ 03-numbers/exercicio18.js
@@ -1,3 +1,5 @@
 // 18. Declare uma constante representando a velocidade da luz e realize uma operação matemática com ela.
 
-const velocidadeDaLuz = 299792458; 
+const velocidadeDaLuz = 299792458;
+
+console.log(velocidadeDaLuz / 12345);
