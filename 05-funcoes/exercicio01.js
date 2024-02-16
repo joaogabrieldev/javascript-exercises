466c0c9fe7b49f0fe5ee79bcaf80ff9d3b0254d2
João Gabriel <dev.joaogabriel@gmail.com>
2024-02-16 18:03:26 -0300

    add exercises functions

--- 05-funcoes/exercicio01.js
+++ 05-funcoes/exercicio01.js
@@ -1,1 +1,7 @@
 // 1. Crie uma função simples que exiba "Olá, mundo!" no console.
+
+function fala() {
+  console.log("Ola mundo");
+}
+
+fala();
--- 05-funcoes/exercicio02.js
+++ 05-funcoes/exercicio02.js
@@ -1,2 +1,7 @@
-
 // 2. Escreva uma função que receba dois números como parâmetros e retorne a soma deles.
+
+function soma(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(soma(20, 45));
