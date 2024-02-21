449527ff7301c0ff3c3e573d8a8a391e538d5855
João Gabriel <dev.joaogabriel@gmail.com>
2024-02-21 05:07:35 -0300

    add exercises

--- 05-funcoes/exercicio07.js
+++ 05-funcoes/exercicio07.js
@@ -1,8 +1,9 @@
 // 7. Implemente uma função que inverta a ordem dos elementos em um array.
 
-let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numeros = [10, 30, 50, 30, 20, 90, 70, 60, 356];
 
 function inverta(array) {
-
-  return 
+  return array.reverse();
 }
+
+console.log(inverta(numeros));
